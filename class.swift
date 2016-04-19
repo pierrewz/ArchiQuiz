eb99f3209ff535f6f8f2e45e04cfee03445e0236
pierrewz <pierrewz@users.noreply.github.com>
2016-04-19 16:59:11 +0200

    Update class.swift

--- class.swift
+++ class.swift
@@ -1,101 +1,72 @@
-//Sample that demonstrates classes and protocols in Swift using a baseball example.
+var points:Int = 0
 
-//Define variables and functions that classes must conform to
-public protocol Hittable {
+public class Question {
+    let photoTitle:String
+    let photoLink:String?
+    let answerName:String
+    let answerLocation:String
+    let answerArchitect:String?
     
-    func hit()
-    func out()
-    func walk()
-    
-    //A variable of type Double with only a getter
-    var battingAverage:Double { get }
-    var onBasePercentage:Double { get }
-    
-}
-
-//An enum lists defines discrete values. A raw value can also be provided
-//for each value, in this case a String.
-public enum Rating:String {
-    case AllStar = "All Star"
-    case BenchWarmer = "Bench Warmer"
-    case MVP = "Most Valuable Player"
-}
-
-// Player class that conforms to Hittable protocol
-public class Player:Hittable, CustomStringConvertible {
-    //Immutable variables are defined with let.
-    //They don't have to be initialized when defined.
-    let name:String
-    
-    //An optional can hold a value or be nil
-    let nickName:String?
-    
-    //Mutable variables are defined with var.
-    var hits = 0
-    var atBats = 0
-    var walks = 0
-
-    //The description property is required by the CustomStringConvertible
-    //protocol, and it provides a textual representation of the object
-    //when printed
-    public var description:String {
-        return "The rating of \(name) is \(rating)."
+    //Initializer
+    public init(photoTitle:String, photoLink:String? = nil, answerName:String, answerLocation:String, answerArchitect:String? = nil) {
+        self.photoTitle = photoTitle
+        self.photoLink = photoLink
+        self.answerName = answerName
+        self.answerLocation = answerLocation
+        self.answerArchitect = answerArchitect
     }
     
-    //An initializer is defined with init. The nickName parameter
-    //has a default value of nil if it is not passed in to the function.
-    public init(name:String, nickName:String? = nil) {
-        //Self is used to distinguish between the class property and
-        //the property passed into the initializer
-        self.name = name
-        self.nickName = nickName
-    }
-    
-    //A computed property calculates rather than stores a value
-    public var battingAverage:Double {
-        return atBats > 0 ? Double(hits) / Double(atBats) : 0
-    }
-    
-    //A computed property calculates rather than stores a value
-    public var onBasePercentage:Double {
-        return atBats > 0 ? (Double(hits) + Double(walks)) / Double(atBats) : 0
-    }
-    
-    public var rating:String {
-        if battingAverage >= 0.3 {
-            if onBasePercentage >= 0.4 {
-                return Rating.MVP.rawValue
-            }
-            return Rating.AllStar.rawValue
+    public func verifyAnswerName(name:String) -> Bool {
+        if self.answerName == name{
+            print("Correct answer, you found the building name!")
+            addPoints(10)
+            return true
+        } else {
+            print("Wrong answer!")
+            return false
         }
-        return Rating.BenchWarmer.rawValue
     }
     
-    public func hit() {
-        hits++
-        atBats++
+    public func verifyAnswerLocation(location:String) -> Bool {
+        if self.answerLocation == location {
+            print("Correct answer, you found the location/city!")
+            addPoints(10)
+            return true
+        } else {
+            print("Wrong answer!")
+            return false
+        }
     }
     
-    public func out() {
-        atBats++
-    }
-    
-    public func walk() {
-        walks++
+    public func verifyAnswerArchitect(architect:String) -> Bool {
+        if self.answerArchitect == architect {
+            print("Correct answer, you found the architect!")
+            addPoints(20)
+            return true
+        } else {
+            print("Wrong answer!")
+            return false
+        }
     }
 }
 
-//Instantiate a player named Swifty
-let p = Player(name:"Swifty", nickName:"Swiftee")
+public func addPoints(morepoints:Int){
+    points += morepoints
+}
 
-print(p.nickName!)
-print(p)
-//Call the out function on the player
-p.out()
-print(p.rating)
-p.out()
-p.hit()
-//The player's rating has improved!
-print(p.rating)
-p.walk()
-print(p.rating)
+public func showPoints(){
+    print("You have",points,"points.")
+}
+
+//Instantiate a question for Eiffel Tower
+let q = Question(photoTitle:"Eiffel Tower", answerName:"Eiffel Tower", answerLocation:"Paris", answerArchitect:"Eiffel")
+
+print(q.photoTitle)
+//show result for name answer
+q.verifyAnswerName("Eiffel Tower")
+//show result for the location
+q.verifyAnswerLocation("Paris")
+//show result for the architect
+q.verifyAnswerArchitect("Gustav Eiffel")
+showPoints()
+
