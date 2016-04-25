15d9f3eaef792621baa91acc9103eed078e2b4e5
pierrewz <pierrewz@users.noreply.github.com>
2016-04-25 14:58:34 +0200

    Update class.swift

--- class.swift
+++ class.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 
-var points:Int = 0
+//var points:Int = 0
 
 //List of suggestions
 var suggestionsName: [String] = ["Empire State Building","Sydney Opera","Burj Dubai","The Shard", "Building1","Building2","Building3"]
@@ -9,6 +9,24 @@ var suggestionsName: [String] = ["Empire State Building","Sydney Opera","Burj Du
 let randomNumber = Int(arc4random_uniform(UInt32(suggestionsName.count)))
 var suggestion:String = suggestionsName[randomNumber]
 print(suggestion)
+
+public class Game {
+    var points:Int = 0
+    //Initializer
+    public init(points:Int? = 0) {
+        self.points = points
+    }
+}
+
+public class Collection {
+    let collectionName:String
+    var listOfQuestions: [Question]?
+    
+     public init(collectionName:String, listOfQuestions:[Question]? = nil) {
+       //TODO
+    }
+    
+}
 
 public class Question {
     let photoTitle:String
@@ -61,12 +79,15 @@ public class Question {
 }
 
 public func addPoints(morepoints:Int){
-    points += morepoints
+    game.points += morepoints
 }
 
 public func showPoints(){
     print("You have",points,"points.")
 }
+
+//Start a new game
+let game = Game(points:0)
 
 //Instantiate a question for Eiffel Tower
 let q = Question(photoTitle:"Eiffel Tower picture", answerName:"Eiffel Tower", answerLocation:"Paris", answerArchitect:"Eiffel")
